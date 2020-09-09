35af8c5c1879af7d1b1f4a347467904e4d7dccee
Andrew Vuong <andrew.vuong@berkeley.edu>
2020-09-09 16:35:26 -0700

    Designed a multi-table schema and loaded into mySQL

--- server/schema.sql
+++ server/schema.sql
@@ -1,9 +1,30 @@
+DROP DATABASE chat;
 CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE user (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  name VARCHAR(50) NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE rooms (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  name VARCHAR(50) NULL,
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  text VARCHAR(150),
+  user_id INTEGER,
+  room_id INTEGER,
+  date DATE NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES user (id),
+  FOREIGN KEY (room_id) REFERENCES rooms (id)
 );
 
 /* Create other tables and define schemas for them here! */
@@ -14,4 +35,13 @@ CREATE TABLE messages (
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
+
+
+
+
+
+
+
+
+
 
